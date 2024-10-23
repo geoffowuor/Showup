4bbad8209a0bde081f501fe4be58806a9c4dde61
jeffx-3 <geoffreygroovy@gmail.com>
2024-10-23 20:42:03 +0300

    configured bootstrap and worked on models

--- events/models.py
+++ events/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=70)
+    photo=models.ImageField(upload_to="media/", null=True)
     description = models.CharField(max_length=420)
-    date = models.DateTimeField
+    date = models.CharField(max_length=60, null=True)
     
-    
-def __str__(self):
-    return self.name
-
+    def __str__(self):
+        return self.name
